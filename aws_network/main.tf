fa14429637cbfea3174b6c9c8e1dbbeb8e6ef51a
igeiman <ec2-user@ip-172-31-84-211.ec2.internal>
2024-02-15 06:54:35 +0000

    Adding my module

--- aws_network/main.tf
+++ aws_network/main.tf
@@ -61,7 +61,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 # Route table to route add default gateway pointing to Internet Gateway (IGW)
-resource "aws_route_table" "public_subnets" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -73,7 +73,7 @@ resource "aws_route_table" "public_subnets" {
 }
 
 # Associate subnets with the custom route table
-resource "aws_route_table_association" "public_routes" {
+resource "aws_route_table_association" "public_route_table_association" {
   count          = length(aws_subnet.public_subnet[*].id)
   route_table_id = aws_route_table.public_route_table.id
   subnet_id      = aws_subnet.public_subnet[count.index].id
